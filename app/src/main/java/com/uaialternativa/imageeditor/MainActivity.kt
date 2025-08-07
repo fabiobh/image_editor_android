122fb1fa4bcdc864efda6dd4d6bd3ca74c4b2e55
fabiobh <psxbrasil@gmail.com>
2025-08-07 20:52:31 -0300

    Add gallery screen UI with image grid and sharing
    Implements the GalleryScreen composable using Jetpack Compose, displaying saved images in a grid with support for selection, sharing, and deletion. Adds required string resources, updates the manifest to include FileProvider for sharing images, and adds file_paths.xml for secure file access. MainActivity now launches the gallery screen as the app's main UI.

--- app/src/main/java/com/uaialternativa/imageeditor/MainActivity.kt
+++ app/src/main/java/com/uaialternativa/imageeditor/MainActivity.kt
@@ -5,12 +5,10 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.uaialternativa.imageeditor.ui.gallery.GalleryScreen
 import com.uaialternativa.imageeditor.ui.theme.ImageEditorTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -21,29 +19,31 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ImageEditorTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                ImageEditorApp()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "1 1 Hello $name!",
-        modifier = modifier
+fun ImageEditorApp() {
+    GalleryScreen(
+        onImageSelected = { savedImage ->
+            // TODO: Navigate to image editor screen
+            // This will be implemented in a future task
+        },
+        onAddImageClicked = {
+            // TODO: Open image picker
+            // This will be implemented in a future task
+        },
+        modifier = Modifier.fillMaxSize()
     )
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun ImageEditorAppPreview() {
     ImageEditorTheme {
-        Greeting("Android")
+        ImageEditorApp()
     }
 }
